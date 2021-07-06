8cba7f180be1621c3d946d43d27ccd2d765b04c8
Joan León <joan.leon@gmail.com>
2021-07-06 18:59:56 +0200

    Add Google Analytics code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
+import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import * as gtag from "../utils/gtag";
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+const Elbec = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      process.env.NODE_ENV === 'production' && gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
+};
+
+export default Elbec;
