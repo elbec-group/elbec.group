536a5e0ddbca4ea096b960dae0b36b56f05aaa14
Joan León <joan.leon@gmail.com>
2021-08-19 17:36:27 +0200

    Fix crossorigin

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <meta name="description" content="Educación lingüística basada en evidencias científicas" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
       </Head>
 
